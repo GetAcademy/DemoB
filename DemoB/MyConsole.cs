20f266a04b4fc66e85ceed6674d7b5e9aabee9c6
GET Academy <terje@getacademy.no>
2022-08-09 11:32:05 +0200

    Initial commit

---
+++ DemoB/MyConsole.cs
@@ -0,0 +1,23 @@
+﻿namespace DemoB
+{
+    internal class MyConsole
+    {
+        public static string Ask(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
+        }
+
+        public static int AskForInt(string question)
+        {
+            var answerStr = Ask(question);
+            return Convert.ToInt32(answerStr);
+        }
+
+        public static bool AskYesOrNo(string question)
+        {
+            var answerStr = Ask(question);
+            return answerStr.ToLower() == "j";
+        }
+    }
+}
---
+++ DemoB/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DemoB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = MyConsole.Ask("Hva heter du? ");
+            Console.WriteLine($"Hei, {name}!");
+
+            int year = MyConsole.AskForInt("I hvilket år er du født? ");
+            int age = 2022 - year;
+
+            bool hadBirthday = MyConsole.AskYesOrNo("Har du hatt bursdag i år (j/n)? ");
+            if (!hadBirthday) age--;
+
+            Console.WriteLine($"Da er du {age} år gammel.");
+        }
+    }
+}
